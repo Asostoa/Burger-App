92fdb0bbc2300f5e4d864e9129dbf5b09e5142bf
alexander de sostoa <alexanderdesostoa@gmail.com>
2020-09-30 11:22:54 -0400

    Adding MVC,Conection,Orm and Server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+let app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+let PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let burger = require('./controllers/burgersController.js')
+
+//Created a simple router with express router from the burgersControllers.js script
+app.use('/', burger)
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
